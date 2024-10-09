9216f472eaab8eca4ca039b6c07231cec2b5b0d0
raph2leretour <tisserandraph@gmail.com>
2024-10-09 16:08:54 +0200

    fish

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias lock="/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/tortureDance.mp4 0 0 1080 1920"
+alias gcl="git clone"
+alias nvim="~/bin/nvim.appimage"
+alias vim="~/bin/nvim.appimage"
