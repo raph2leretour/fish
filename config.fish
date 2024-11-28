aa685f8dddd20399add9859cea2351afa4fed2eb
raph2leretour <tisserandraph@gmail.com>
2024-11-28 20:27:56 +0100

    config.fish--> adding the new path for starship

--- config.fish
+++ config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias lock="/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/tortureDance.mp4 0 0 1080 1920"
-alias gcl="git clone"
-alias nvim="~/bin/nvim.appimage"
-alias vim="~/bin/nvim.appimage"
+
+set -Ux STARSHIP_CONFIG ~/.config/starship/starship.toml
+
+alias lock "/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/tortureDance.mp4 0 0 1080 1920"
+alias gcl "git clone"
+alias nvim "~/bin/nvim.appimage"
+alias vim "~/bin/nvim.appimage"
+
+starship init fish | source
