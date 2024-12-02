9e432de9ebcd22577e4756826fa08ae7ca0c23f0
raph2leretour <tisserandraph@gmail.com>
2024-12-02 16:35:49 +0100

    sdlfj

--- config.fish
+++ config.fish
@@ -5,6 +5,7 @@ end
 set -Ux STARSHIP_CONFIG ~/.config/starship/starship.toml
 
 alias lock "/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/tortureDance.mp4 0 0 1080 1920"
+alias lock2 "/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/paprikaIntro.mp4 0 0 1080 1920"
 alias gcl "git clone"
 alias nvim "~/bin/nvim.appimage"
 alias vim "~/bin/nvim.appimage"
