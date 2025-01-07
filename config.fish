7e4d0706b57b86ae7dd98de433bb6cd1a4a97e4e
raph2leretour <tisserandraph@gmail.com>
2025-01-07 20:32:07 +0100

    just discovered abbr

--- config.fish
+++ config.fish
@@ -6,8 +6,10 @@ set -Ux STARSHIP_CONFIG ~/.config/starship/starship.toml
 
 alias lock "/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/tortureDance.mp4 0 0 1080 1920"
 alias lock2 "/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/paprikaIntro.mp4 0 0 1080 1920"
-alias gcl "git clone"
 alias nvim "~/bin/nvim.appimage"
 alias vim "~/bin/nvim.appimage"
+
+abbr gcl git clone
+abbr lla ls -la
 
 starship init fish | source
