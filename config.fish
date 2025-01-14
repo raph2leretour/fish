46c7f9b62b10ee5ba89742aaba83430ae53b030c
Raph <rtissera@student.42.fr>
2025-01-14 15:45:34 +0100

    abbr vim = nvim

--- config.fish
+++ config.fish
@@ -6,10 +6,9 @@ set -Ux STARSHIP_CONFIG ~/.config/starship/starship.toml
 
 alias lock "/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/tortureDance.mp4 0 0 1080 1920"
 alias lock2 "/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock/pimp_my_lock.sh ~/Videos/paprikaIntro.mp4 0 0 1080 1920"
-alias nvim "~/bin/nvim.appimage"
-alias vim "~/bin/nvim.appimage"
 
 abbr gcl git clone
 abbr lla ls -lA
+abbr vim nvim
 
 starship init fish | source
